4cfbb41451bdbaaa89bc7120fd5f8796a76df407
schuer <ds@decaf.de>
2019-05-19 17:59:12 +0200

    fix settings page

--- plugins/dragndrop/pages/index.php
+++ plugins/dragndrop/pages/index.php
@@ -1,12 +1,14 @@
 <?php
-$plugin = $this->getPlugin('dragndrop');
+/**
+ * @var rex_plugin $this
+ */
 
 if (rex_post('config-submit', 'boolean')) {
-    $plugin->setConfig(rex_post('config', [
+    $this->setConfig(rex_post('config', [
         ['display_sort_buttons', 'bool']
     ]));
 
-    echo rex_view::success($plugin->i18n('saved'));
+    echo rex_view::success($this->i18n('saved'));
 }
 
 $content = '<fieldset>';
@@ -14,8 +16,8 @@ $content = '<fieldset>';
 $formElements = [];
 
 $n = [];
-$n['label'] = '<label for="rex-bloecks-dragndrop-display_sort_buttons">' . $plugin->i18n('display_sort_buttons') . '</label>';
-$n['field'] = '<input type="checkbox" id="rex-bloecks-dragndrop-display_sort_buttons" name="config[display_sort_buttons]" value="1" ' . ($plugin->getConfig('display_sort_buttons') ? ' checked="checked"' : '') . ' />';
+$n['label'] = '<label for="rex-bloecks-dragndrop-display_sort_buttons">' . $this->i18n('display_sort_buttons') . '</label>';
+$n['field'] = '<input type="checkbox" id="rex-bloecks-dragndrop-display_sort_buttons" name="config[display_sort_buttons]" value="1" ' . ($this->getConfig('display_sort_buttons') ? ' checked="checked"' : '') . ' />';
 $formElements[] = $n;
 
 $fragment = new rex_fragment();
@@ -25,7 +27,7 @@ $content .= $fragment->parse('core/form/checkbox.php');
 $formElements = [];
 
 $n = [];
-$n['field'] = '<button class="btn btn-save rex-form-aligned" type="submit" name="config-submit" value="1" ' . rex::getAccesskey($plugin->i18n('save'), 'save') . '>' . $plugin->i18n('save') . '</button>';
+$n['field'] = '<button class="btn btn-save rex-form-aligned" type="submit" name="config-submit" value="1" ' . rex::getAccesskey($this->i18n('save'), 'save') . '>' . $this->i18n('save') . '</button>';
 $formElements[] = $n;
 
 $fragment = new rex_fragment();
@@ -35,7 +37,7 @@ $buttons = $fragment->parse('core/form/submit.php');
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit');
-$fragment->setVar('title', $plugin->i18n('settings'));
+$fragment->setVar('title', $this->i18n('settings'));
 $fragment->setVar('body', $content, false);
 $fragment->setVar('buttons', $buttons, false);
 $content = $fragment->parse('core/page/section.php');
