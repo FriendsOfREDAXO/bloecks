f8b7e23452aef184c92664a2a9a853f34c64cc7f
schuer <ds@decaf.de>
2019-10-31 10:09:02 +0100

    build: fix release zip flow
    - build for production first
    - exclude sourcemaps

--- zip.sh
+++ zip.sh
@@ -4,14 +4,14 @@ SRCDIR=$( cd "$( dirname "$SCRIPT_DIR/./" )" && pwd );
 DSTDIR=$( cd "$( dirname "$SCRIPT_DIR/../" )" && pwd );
 NAME=${SCRIPT_DIR##*/}
 
+# build for production
+grunt --production
+
 cd $SCRIPT_DIR/..;
 
 if [ -f "$NAME.zip" ];then
     rm -f "$NAME.zip";
 fi;
 
-# build for production
-grunt --production
-
 # generate release zip file
-zip -r "$NAME.zip" "./$NAME/" -x "*node_modules*" "*.git*" "rsync*" "*Gruntfile*" "*package.json" "*assets_src*" "*.sh" "*.exclude" "*.DS*" "*._*"
+zip -r "$NAME.zip" "./$NAME/" -x "*node_modules*" "*.git*" "rsync*" "*Gruntfile*" "*package.json" "*assets_src*" "*.sh" "*.exclude" "*.DS*" "*._*" "*.css.map" "*.js.map"
