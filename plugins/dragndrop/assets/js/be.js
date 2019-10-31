c7fb0ba62a220797263fe98bcd0babe3ee1e93ba
schuer <ds@decaf.de>
2019-10-31 09:36:42 +0100

    chore: compile assets

--- plugins/dragndrop/assets/js/be.js
+++ plugins/dragndrop/assets/js/be.js
@@ -1,2 +1,120 @@
-bloecks.addPlugin("dragndrop",{init:function(){var i=this;$(".rex-slices:not(.is--undraggable)").each(function(e,t){try{$(t).sortable("destroy")}catch(e){}$(t).find(".rex-slice.rex-slice-edit, .rex-slice.rex-slice-add").length?$(t).addClass("is--editing"):i.addSortables(t)})},markDisabledItems:function(e,i){i="string"!=typeof i?"ui-state-disabled":i,$(e).find(".rex-slice-output:not(."+i+")").each(function(e,t){$(t).find('[href*="direction=move"]').length||$(t).addClass(i)})},addSortables:function(e){var t="ui-state-disabled";this.markDisabledItems(e,t),$(e).sortable({appendTo:document.body,handle:".rex-page-section>.panel>.panel-heading",placeholder:"rex-slice rex-slice-placeholder",cancel:t,helper:"clone",items:">.rex-slice.rex-slice-draggable",create:function(){$(e).css({minHeight:$(e).outerHeight()})},start:function(e,t){$(this).addClass("ui-state-sorting"),$(this).sortable("refreshPositions"),t.placeholder.height(t.helper.outerHeight())},stop:function(){$(this).hasClass("ui-state-updated")||$(this).removeClass("ui-state-sorting")},update:function(e,t){$(this).addClass("ui-state-updated"),$(this).sortable("refresh");var i=t.position.top<t.originalPosition.top?"up":"down",s=bloecks.getSliceId(t.item),l=t.item.prevAll(".rex-slice-draggable").length?bloecks.getSliceId(t.item.prevAll(".rex-slice-draggable").first()):0;if(null!==s&&null!==l){var a=t.item.find('[href*="direction=move'+i+'"]').length?t.item.find('[href*="direction=move'+i+'"]').first().attr("href"):null;null!==a?(a=(a=(a=a.replace(/(&amp;|&)direction=move(up|down)/,"$1direction=move$2$1insertafter="+l)).replace(/content_move_slice/,"content_move_slice_to")).replace(/_csrf_token=[^&]+/,"_csrf_token="+t.item.data("csrf-token")),a+="#slice"+s):a=window.location.href,bloecks.executePjax(a)}}})}});
-//# sourceMappingURL=be.js.map
+bloecks.addPlugin(
+    'dragndrop',
+    {
+        init : function()
+        {
+            var _this = this;
+            // for each rex-slices container
+            $('.rex-slices:not(.is--undraggable)').each(function(i, slicewrapper)
+            {
+                // remove any sortable
+                try {
+                    $(slicewrapper).sortable('destroy');
+                } catch(ev) { }
+
+                if(!$(slicewrapper).find('.rex-slice.rex-slice-edit, .rex-slice.rex-slice-add').length)
+                {
+                    // only add sortables if the page is not in EDIT mode
+                    _this.addSortables(slicewrapper);
+                }
+                else
+                {
+                    $(slicewrapper).addClass('is--editing');
+                }
+            });
+        },
+
+        markDisabledItems : function(slicewrapper, disabledClass)
+        {
+            disabledClass = typeof(disabledClass) != 'string' ? 'ui-state-disabled' : disabledClass;
+
+            $(slicewrapper).find('.rex-slice-output:not(.' + disabledClass + ')').each(function(j, slice)
+            {
+                if(!$(slice).find('[href*="direction=move"]').length)
+                {
+                    // we won't let the user move items that cannot be moved by perms etc.
+                    $(slice).addClass(disabledClass);
+                }
+            });
+        },
+
+        addSortables : function(slicewrapper)
+        {
+            var disabledClass = 'ui-state-disabled';
+
+            this.markDisabledItems(slicewrapper, disabledClass);
+
+            $(slicewrapper).sortable({
+                appendTo: document.body,
+                handle: '.rex-page-section>.panel>.panel-heading',
+                placeholder: 'rex-slice rex-slice-placeholder',
+                cancel: disabledClass,
+                // containment: $(slicewrapper),
+                helper: 'clone',
+                items: '>.rex-slice.rex-slice-draggable',
+
+                create: function (event, ui)
+                {
+                    // fix wrapper height to avoid page jumps
+                    $(slicewrapper).css({
+                        minHeight: $(slicewrapper).outerHeight()
+                    });
+                },
+
+                start : function(event, ui)
+                {
+                    $(this).addClass('ui-state-sorting');
+
+                    // refresh positions just to make sure everything is okay
+                    $(this).sortable('refreshPositions');
+
+                    // set placeholder height according to item (helper)
+                    ui.placeholder.height(ui.helper.outerHeight());
+                },
+
+                stop : function(event, ui)
+                {
+                    if(!$(this).hasClass('ui-state-updated'))
+                    {
+                        $(this).removeClass('ui-state-sorting');
+                    }
+                },
+
+                update : function(event, ui)
+                {
+                    $(this).addClass('ui-state-updated');
+
+                    // refresh items just to make sure everything is okay
+                    $(this).sortable('refresh');
+
+                    var direction = ui.position.top < ui.originalPosition.top ? 'up' : 'down';
+
+                    var this_id = bloecks.getSliceId(ui.item),
+                        prev_id = ui.item.prevAll('.rex-slice-draggable').length ? bloecks.getSliceId(ui.item.prevAll('.rex-slice-draggable').first()) : 0;
+
+                    if(this_id !== null && prev_id !== null)
+                    {
+                        console.log('Move ' + this_id + ' ' + direction + ', after ' + prev_id);
+
+                        var url = ui.item.find('[href*="direction=move' + direction + '"]').length ? ui.item.find('[href*="direction=move' + direction + '"]').first().attr('href') : null;
+                        if(url !== null)
+                        {
+                            url = url.replace(/(&amp;|&)direction=move(up|down)/, "$1direction=move$2$1insertafter=" + prev_id);
+                            url = url.replace(/content_move_slice/, "content_move_slice_to");
+                            url = url.replace(/_csrf_token=[^&]+/, "_csrf_token=" + ui.item.data('csrf-token'));
+                            url += '#slice' + this_id;
+                        }
+                        else
+                        {
+                            url = window.location.href;
+                        }
+
+                        console.log(url);
+
+                        bloecks.executePjax(url);
+                    }
+                }
+            });
+        },
+    }
+);
--- plugins/status/assets/js/be.js
+++ plugins/status/assets/js/be.js
@@ -1,2 +1,38 @@
-bloecks.addPlugin("status",{init:function(){$('[href*="bloecks/status/status"][href*="status="]').each(function(t,s){var e=$(s).attr("href").match(/status=(\d)/),a=$(s).parents(".rex-slice-output");a.length&&e&&((e=0===parseInt(e[1]))?a.first().removeClass("bloecks--status--inactive"):a.first().addClass("bloecks--status--inactive")),$(s).on("click.bloecks",function(){return $.pjax({url:$(this).attr("href"),container:"#rex-js-page-main-content",fragment:"#rex-js-page-main-content",push:!1}),!1})})}});
-//# sourceMappingURL=be.js.map
+bloecks.addPlugin(
+    'status',
+    {
+        init : function()
+        {
+            $('[href*="bloecks/status/status"][href*="status="]').each(function(i, btn){
+                var setStatus = $(btn).attr('href').match(/status=(\d)/),
+                    sliceContainer = $(btn).parents('.rex-slice-output');
+
+                if(sliceContainer.length && setStatus)
+                {
+                    setStatus = parseInt(setStatus[1]) === 0;
+                    if(setStatus)
+                    {
+                        sliceContainer.first().removeClass('bloecks--status--inactive');
+                    }
+                    else
+                    {
+                        sliceContainer.first().addClass('bloecks--status--inactive');
+                    }
+                }
+
+                $(btn).on('click.bloecks', function(e){
+
+                    $.pjax(
+                    {
+                        url: $(this).attr('href'),
+                        container: '#rex-js-page-main-content',
+                        fragment : '#rex-js-page-main-content',
+                        push : false
+                    });
+
+                    return false;
+                })
+            });
+        }
+    }
+);
