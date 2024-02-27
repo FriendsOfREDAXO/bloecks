b7237f7fd0a129c2347c5f4a6f9d24bb5ef0a976
Thomas Skerbis <thomas.skerbis@mac.com>
2024-02-27 13:01:07 +0100

    php Optimierungen

--- lib/Bloecks.php
+++ lib/Bloecks.php
@@ -1,41 +1,40 @@
 <?php
 /**
- * bloecks class - basic functions for the addon and its plugins.
+ * Bloecks-Klasse - Grundfunktionen für das AddOn und seine Plugins.
  */
 class bloecks extends bloecks_abstract
 {
     /**
-     * Initializes the addon.
+     * Initialisiert das AddOn.
      */
     public static function init(rex_extension_point $ep)
     {
+        // Bei Zugriff im Backend und wenn ein Benutzer angemeldet ist, Backend-Funktionen initialisieren
         if (rex::isBackend() && rex::getUser()) {
-            // initialize the backend functions
             bloecks_backend::init($ep);
-        } elseif (!rex::isBackend()) {
-            // things to do in frontend
+            return;
+        }
+
+        // Im Frontend, eigenen Extension-Point registrieren
+        if (!rex::isBackend()) {
             rex_extension::register('SLICE_SHOW', ['bloecks', 'showSlice'], rex_extension::EARLY);
         }
     }
 
     /**
-     * Creates our own extension point to use in all our plugins.
+     * Erstellt einen eigenen Extension Point, der in allen unseren Plugins verwendet wird.
      *
-     * @return string slice content
+     * @return string Inhalte des Slices
      */
     public static function showSlice(rex_extension_point $ep)
     {
-        // get subject
-        $slice_content = $ep->getSubject();
-
-        // add our own extension point
+        // Subject erhalten und unseren eigenen Extension Point hinzufügen
         $slice_content = rex_extension::registerPoint(new rex_extension_point(
             'SLICE_SHOW_BLOECKS_FE',
-            $slice_content,
+            $ep->getSubject(),
             $ep->getParams()
         ));
 
-        // return
         return $slice_content;
     }
 }
