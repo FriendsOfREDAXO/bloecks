0ff0e27c718bfaf7aab4b8263fb3e33ec4bb083b
Thomas Skerbis <thomas.skerbis@mac.com>
2025-08-14 10:56:59 +0200

    fix console
    fixed: https://github.com/FriendsOfREDAXO/bloecks/issues/144

--- boot.php
+++ boot.php
@@ -3,9 +3,9 @@
  * BLOECKS - Einfaches Drag & Drop + Copy/Paste für REDAXO
  */
 
+use FriendsOfRedaxo\Bloecks\Api;
 use FriendsOfRedaxo\Bloecks\Backend;
 use FriendsOfRedaxo\Bloecks\Wrapper;
-use FriendsOfRedaxo\Bloecks\Api;
 
 // Register API explicitly
 rex_api_function::register('bloecks', Api::class);
@@ -17,19 +17,21 @@ rex_perm::register('bloecks[copy]');
 rex_perm::register('bloecks[order]');
 
 // Backend functionality
-if (rex::isBackend()) {
-    // Clear clipboard on login/logout and session start for security
-    Backend::clearClipboardOnSessionStart();
-    
-    Backend::init();
-    
-    // Register wrapper for slice_columns-style drag & drop only if enabled
-    $addon = rex_addon::get('bloecks');
-    if ($addon->getConfig('enable_drag_drop', false)) {
-        rex_extension::register('SLICE_SHOW', Wrapper::addDragDropWrapper(...), rex_extension::EARLY);
-        rex_extension::register('SLICE_MENU', Wrapper::addDragHandle(...));
-        # error_log("BLOECKS DEBUG: Drag & Drop extension points registered");
-    } else {
-        # error_log("BLOECKS DEBUG: Drag & Drop disabled, no wrapper extension points registered");
+if (rex::isBackend() && PHP_SAPI !== 'cli') {
+    // Only run session-dependent code when not in CLI context
+    rex_extension::register('PACKAGES_INCLUDED', function () {
+        // Clear clipboard on login/logout and session start for security
+        Backend::clearClipboardOnSessionStart();
+        Backend::init();
+
+        // Register wrapper for slice_columns-style drag & drop only if enabled
+        $addon = rex_addon::get('bloecks');
+        if ($addon->getConfig('enable_drag_drop', false)) {
+            rex_extension::register('SLICE_SHOW', Wrapper::addDragDropWrapper(...), rex_extension::EARLY);
+            rex_extension::register('SLICE_MENU', Wrapper::addDragHandle(...));
+            # error_log("BLOECKS DEBUG: Drag & Drop extension points registered");
+        } else {
+            # error_log("BLOECKS DEBUG: Drag & Drop disabled, no wrapper extension points registered");
+        }
     }
 }
